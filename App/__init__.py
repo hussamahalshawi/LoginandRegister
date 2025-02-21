09012970565d60892e9b6090e9bb0107b185a7f7
Hussam Alshawi <hussamahalshawi@gmail.com>
2025-02-21 13:29:09 +0200

    Add more file project App __init__

---
+++ App/__init__.py
@@ -0,0 +1,16 @@
+
+from flask import Flask
+from flask_pymongo import PyMongo
+
+mongo = PyMongo()
+
+def create_app(Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    mongo.init_app(app)
+    app.secret_key = 'your_secret_key'
+    # تسجيل البلوبرينت
+    from .routes import auth
+    app.register_blueprint(auth)
+
+    return app
