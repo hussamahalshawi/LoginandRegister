246599b403947715ded2282995ea16d33a3cee21
Hussam Alshawi <hussamahalshawi@gmail.com>
2025-07-11 23:35:41 +0300

    deploy

--- App/__init__.py
+++ App/__init__.py
@@ -5,7 +5,7 @@ from flask_pymongo import PyMongo
 mongo = PyMongo()
 
 def create_app(Config):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', template_folder='App/templates')
     app.config.from_object(Config)
     mongo.init_app(app)
     app.secret_key = 'your_secret_key'
