5ddb21491459f72b486896d5fca37c2d580721e1
Hussam Alshawi <hussamahalshawi@gmail.com>
2025-07-12 22:12:58 +0300

    edit MONGO_URI for deploy

--- config/default.py
+++ config/default.py
@@ -1,6 +1,8 @@
+import os
 
 
 class Config:
-    MONGO_URI = "mongodb://localhost:27017/loginregister"
+    MONGO_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/loginregister')
+    # MONGO_URI = "mongodb://localhost:27017/loginregister"
     DEBUG = True
 
