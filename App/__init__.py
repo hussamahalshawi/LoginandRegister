4be1e8a2c2937b86ef84d426001ea4edb05f6626
Hussam Alshawi <hussamahalshawi@gmail.com>
2025-07-13 11:35:22 +0300

    print(mongo.db.list_collection_names())

--- App/__init__.py
+++ App/__init__.py
@@ -4,9 +4,9 @@ from flask_pymongo import PyMongo
 
 mongo = PyMongo()
 
-def create_app(Config):
+def create_app(config_class):
     app = Flask(__name__, static_folder='../static')
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
     mongo.init_app(app)
     app.secret_key = 'your_secret_key'
     # تسجيل البلوبرينت
--- config/config.py
+++ config/config.py
@@ -3,6 +3,7 @@ import os
 
 class Config:
     MONGO_URI = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/loginregister')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
     # MONGO_URI = "mongodb://localhost:27017/loginregister"
     DEBUG = True
 
